042ca0b734902e800680555b6fb86bf9491a5b5b
Franco Jesus Cahua Soto <fcahua@unsa.edu.pe>
2025-05-16 18:54:00 -0500

    Lab01 finished

--- lab01/Ejercicio1.java
+++ lab01/Ejercicio1.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.math.*;
 
 public class Ejercicio1{
 
@@ -7,6 +8,7 @@ public class Ejercicio1{
     public static double calcularMediana(int[]calificaciones){
         int numEstudiantes;
         double mediana;
+        calificaciones = ordenarArray(calificaciones);
         numEstudiantes = calificaciones.length;
         if(numEstudiantes % 2 ==0){
             mediana = (calificaciones[(numEstudiantes/2)-1]+calificaciones[(numEstudiantes/2)])/2.0;
@@ -17,17 +19,73 @@ public class Ejercicio1{
         return mediana;
     }
 
+    public static int mayor(int[]numeros){
+        int mayor=numeros[0];
+        int pos=0;
+        for (int i=0; i<numeros.length;i++){
+            if (numeros[i]>mayor){
+                mayor = numeros[i];
+                pos =i;
+            }
+        }
+        return pos;
+    }
+    public static int [] ordenarArray(int[]array){
+        int auxiliar;
+        for(int i=0;i<array.length;i++){
+            for(int j=0; j<array.length-i-1;j++){
+                if(array[j]>array[j+1]){
+                auxiliar = array[j];
+                array[j] = array[j+1];
+                array[j+1]=auxiliar;
+            }
+            }
+        }
+        return array;
+    }
+
 
     public static int calcularModa(int [] calificaciones){
-        int [] frecuencia, buscados;
-        frecuencia = new int[calificaciones.length];
-        buscados = new int[calificaciones.length];
-        for(int i =0;i<calificaciones.length;i++){
-            int actual = calificaciones[i];
-            for (int j=0; j<caloi)
+        ArrayList<Integer> indices=new ArrayList<>();
+        int numCalificaciones, calificacionActual;
+        numCalificaciones= calificaciones.length;
+        boolean create;
+        int[] frecuencia = new int[numCalificaciones];
 
-
+        for (int i=0; i<numCalificaciones;i++){
+            create=true;
+            calificacionActual = calificaciones[i];
+            for (int j =0; j<indices.size();j++){
+                if (calificacionActual == indices.get(j)){
+                    frecuencia[j]+=1;
+                    create = false;
+                }
+            }
+            if (create){
+                indices.add(calificacionActual);
+                frecuencia[indices.size() - 1] = 1;
+            }
         }
+
+        return indices.get(mayor(frecuencia));
+    
+    }
+    public static double calcularMedia(int[]calificaciones){
+        double suma =0;
+        int numElementos =calificaciones.length;
+        for(int i=0;i<calificaciones.length;i++){
+            suma=suma+calificaciones[i];
+        }
+        return suma/numElementos;
+    }
+    public static double calcularDesviacionEstandar(int[] calificaciones){
+        double suma, media;
+        suma =0;
+        media = calcularMedia(calificaciones);
+        for (int i=0;i<calificaciones.length;i++){
+            suma= suma +Math.pow((calificaciones[i]-media), 2);
+        }
+        return Math.sqrt(suma/calificaciones.length);
 
     }
     public static void main(String[] args) {
@@ -38,22 +96,24 @@ public class Ejercicio1{
 
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Ingrese el número de estudiantes: ");
+        System.out.print("Ingrese el número de estudiantes: ");
         numEstudiantes = scan.nextInt();
         int [] calificaciones = new int[numEstudiantes];
 
 
         for(int i =0; i<numEstudiantes;i++){
+            System.out.println("Ingrese la calificacion del estudiante número "+(i+1));
             nota = scan.nextInt();
             calificaciones[i] = nota;
         }
 
 
+        moda = calcularModa(calificaciones);
         mediana = calcularMediana(calificaciones);
-        System.err.println(mediana);
-
-
-       
+        desviacionEstandar= calcularDesviacionEstandar(calificaciones);
+        System.err.println("Moda: "+moda);
+        System.err.println("Mediana: "+mediana);
+        System.err.println("Desviacion estándar: "+desviacionEstandar);
        
     }
 }
